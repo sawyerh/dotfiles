02b1366bbb7e525670aaf3de68404ab7df5100e9
Sawyer Hollenshead <git@sawyerh.com>
2020-04-13 13:53:57 -0400

    Init pyenv

--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -6,14 +6,18 @@ if  which brew > /dev/null; then
     # Load Azure CLI completions
     azCompletions="$(brew --prefix)/etc/bash_completion.d/az"
     [ -s $azCompletions ] && source $azCompletions
-
+    
     # Load zsh-autosuggestions
     # https://github.com/zsh-users/zsh-autosuggestions
     autosuggestionsPath="$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
     [ -s $autosuggestionsPath ] && source $autosuggestionsPath
-
+    
     # zsh-syntax-highlighting.zsh wraps ZLE widgets. It must be sourced after all
     # custom widgets have been created
     syntaxHighlightingPath="$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
     [ -s $syntaxHighlightingPath ] && source $syntaxHighlightingPath
+fi;
+
+if  which pyenv > /dev/null; then
+    eval "$(pyenv init -)"
 fi;
