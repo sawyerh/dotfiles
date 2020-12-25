1c7bbc345416d8ad5ab0efb027f3263257e94ccf
Sawyer Hollenshead <git@sawyerh.com>
2020-12-25 12:15:35 -0500

    Remove azcopy

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -4,10 +4,6 @@ export NVM_DIR="$HOME/.nvm"
 # Tell the system to use programs installed by Homebrew:
 export PATH="/usr/local/sbin:$PATH"
 
-# Make `azcopy` command available from any directory
-# Download from: https://github.com/Azure/azure-storage-azcopy
-export PATH="~/Dev/SDK/azcopy:$PATH"
-
 # Python package manager used on PFML
 # https://python-poetry.org/docs/#installation
 export PATH="$HOME/.poetry/env:$PATH"
