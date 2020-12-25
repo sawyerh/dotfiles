1fb7f35c1ebd002b8f0579508184c212be188a77
Sawyer Hollenshead <git@sawyerh.com>
2020-12-25 12:14:44 -0500

    Remove azure cli bash completion setup

--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -1,12 +1,4 @@
-# Need this for Azure CLI bash completions
-# https://github.com/Azure/azure-cli/issues/1722#issuecomment-370205593
-autoload bashcompinit && bashcompinit
-
 if  which brew > /dev/null; then
-    # Load Azure CLI completions
-    azCompletions="$(brew --prefix)/etc/bash_completion.d/az"
-    [ -s $azCompletions ] && source $azCompletions
-    
     # Load zsh-autosuggestions
     # https://github.com/zsh-users/zsh-autosuggestions
     autosuggestionsPath="$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
